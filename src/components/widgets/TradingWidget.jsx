2b40e58653219a7840925274980b9fd1151f0587
Nahid Hossain <nahid95622@gmail.com>
2025-08-09 16:18:35 +0600

    updated heatmap and final

--- src/components/widgets/TradingWidget.jsx
+++ src/components/widgets/TradingWidget.jsx
@@ -1,16 +1,19 @@
 "use client";
-import React, { useLayoutEffect, useRef, memo } from "react";
+import React, { useEffect, useRef, memo } from "react";
 
 function TradingViewWidget() {
   const container = useRef();
 
-  useLayoutEffect(() => {
+  useEffect(() => {
+
+    if (!container.current) return;
+
     const script = document.createElement("script");
     script.src =
       "https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js";
     script.type = "text/javascript";
     script.async = true;
-    script.innerHTML = `
+    script.textContent  = `
       {
         "dataSource": "SPX500",
         "blockSize": "market_cap_basic",
@@ -37,9 +40,9 @@ function TradingViewWidget() {
 
   return (
     <div className="w-full overflow-x-auto">
-      <div className="flex justify-center min-w-[600px] sm:min-w-[800px] md:min-w-[1000px] lg:min-w-[1200px]">
+      <div className="flex justify-center min-w-[360px] sm:min-w-[600px] md:min-w-[800px] md:px-24 lg:min-w-[1200px]">
         <div
-          className="tradingview-widget-container w-full aspect-[12/5]"
+          className="tradingview-widget-container w-full aspect-[12/5] min-h-[300px]"
           ref={container}
         >
           <div className="tradingview-widget-container__widget" />
